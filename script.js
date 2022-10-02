0b145ddfb144b0a7078c03c5838d3a52ec51d4d2
Jared <jaredcote7@gmail.com>
2022-10-02 09:57:25 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const h2 = document.createElement("h2");
+h2.textContent = "United States";
+document.querySelector("body").appendChild(h2);
+h2. style. textAlign = 'center';
+h2. style. color='yellow';
+h2. style. fontFamily='Courier New', Courier, monospace;
